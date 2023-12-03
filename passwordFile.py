7d8acfd28cc6e18bb998247a75809e40a4329246
andre-Hazim <andre.hazim1@gmail.com>
2023-12-03 17:41:43 -0500

    fix

--- passwordFile.py
+++ passwordFile.py
@@ -2,12 +2,14 @@ import os
 import bcrypt
 from accessMatrix import Roles
 from typing import List
+
+
 class User:
-    def __init__(self, username:str, password:str, group:Roles, user_id:int, salt=bcrypt.gensalt()):
+    def __init__(self, username: str, password: str, group: Roles, user_id: int, salt=bcrypt.gensalt()):
         self.username = username
         self.salt = salt
         self.salted_hash = bcrypt.hashpw(password.encode('utf-8'), self.salt)
-        self.group= group
+        self.group = group
         self.user_id = user_id
         self.home_dir = f'/home/{self.username}'
 
@@ -18,19 +20,20 @@ class User:
     def to_string(self):
         return f"{self.username}:{self.salt.decode('utf-8')}:{self.salted_hash.decode('utf-8')}:{self.group}:{self.user_id}:{self.home_dir}"
 
-def read_password_file(file_path)-> List[User]:
+
+def read_password_file(file_path) -> List[User]:
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
     users = []
-    if not len(lines) ==0:
+    if not len(lines) == 0:
         for line in lines:
             parts = line.strip().split(':')
             username, salt, salted_hash, group, user_id, homedir = parts
             group_Enum = None
             for r in Roles:
                 if r.value == group:
-                    group_Enum= r
+                    group_Enum = r
             user = User(username, '', group_Enum, user_id)
             user.salt = salt.encode('utf-8')
             user.salted_hash = salted_hash.encode('utf-8')
@@ -40,9 +43,8 @@ def read_password_file(file_path)-> List[User]:
     else:
         return []
 
-def write_pass_word_file(users, passfile= "passwd.txt"):
 
-    
+def write_pass_word_file(users, passfile="passwd.txt"):
     # Check if the file exists; if not, create it
     if not os.path.isfile(passfile):
         open(passfile, 'w').close()
@@ -54,5 +56,3 @@ def write_pass_word_file(users, passfile= "passwd.txt"):
     with open(passfile, "a") as file:
         for user in users:
             file.write(user.to_string() + "\n")
-
-
